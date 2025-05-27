00bea0a2d47f07fe3eb625ccd0f0adb0ab6ebb5b
shamanthhiremath <shamanth.hiremath.101@gmail.com>
2025-05-28 01:31:23 +0530

    azure deployemnet using code

--- server/index.js
+++ server/index.js
@@ -33,6 +33,14 @@ app.use(
 app.use('/api/admin', adminRoutes);
 app.use('/api/events', eventRoutes);
 
+// Serve static files from the React app build
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// For any other route, serve the React app's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+});
+
 // Health check endpoint
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'OK', message: 'Server is running' });
